265d510d30bec453489d04e14f17572b64ba4c7c
ngnushi <148450157+ngnushi@users.noreply.github.com>
2023-11-23 21:04:10 +0100

    Update load_raw_data.sh

--- sqlDocker/load_raw_data.sh
+++ sqlDocker/load_raw_data.sh
@@ -6,3 +6,15 @@ docker exec sqlserver /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Ih4teMic
 # Add tax rate
 echo ADDING TAX RATE
 docker exec sqlserver /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Ih4teMicrosoft# -d dwh -i sql_files/add_tax_rate_raw.sql
+#Add time
+echo ADDING TIME
+docker exec sqlserver /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Ih4teMicrosoft# -d dwh -i sql_files/add_time_raw.sql
+#Add date
+echo ADDING DATE
+docker exec sqlserver /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Ih4teMicrosoft# -d dwh -i sql_files/add_date_raw.sql
+#Add daily market
+echo ADDING DAILY MARKET
+docker exec sqlserver /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Ih4teMicrosoft# -d dwh -i sql_files/add_daily_market_raw.sql
+#Add cash transaction
+echo ADDING CASH TRANSACTION
+docker exec sqlserver /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Ih4teMicrosoft# -d dwh -i sql_files/add_cash_transaction_raw.sql
