510842a11020268f96cb37645a542a29edf25ff0
Gabriel Lozano <golozanop@unal.edu.co>
2023-11-24 17:55:04 +0100

    adds trade and trade history

--- sqlDocker/load_raw_data.sh
+++ sqlDocker/load_raw_data.sh
@@ -6,3 +6,9 @@ docker exec sqlserver /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Ih4teMic
 # Add tax rate
 echo ADDING TAX RATE
 docker exec sqlserver /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Ih4teMicrosoft# -d dwh -i sql_files/add_tax_rate_raw.sql
+# Add trade history
+echo ADDING TRADE HISTORY
+docker exec sqlserver /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Ih4teMicrosoft# -d dwh -i sql_files/add_trade_type_raw.sql
+# Add Trade
+echo ADDING TRADE
+docker exec sqlserver /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Ih4teMicrosoft# -d dwh -i sql_files/add_trade_raw.sql
