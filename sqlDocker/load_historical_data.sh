a66806730e1596082d233ea4f21b6ae3d8412247
ngnushi <148450157+ngnushi@users.noreply.github.com>
2023-11-27 16:29:06 +0100

    Update load_historical_data.sh
    Delete add_time_historical and add_date_historical

--- sqlDocker/load_historical_data.sh
+++ sqlDocker/load_historical_data.sh
@@ -3,12 +3,6 @@
 # Adds Tax Rate
 echo ADDING TAX RATE
 docker exec sqlserver /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Ih4teMicrosoft# -d dwh -i sql_files/add_tax_rate_historical.sql
-# Adds Time
-echo ADDING TIME
-docker exec sqlserver /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Ih4teMicrosoft# -d dwh -i sql_files/add_time_historical.sql
-#Adds Date
-echo ADDING DATE
-docker exec sqlserver /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Ih4teMicrosoft# -d dwh -i sql_files/add_date_historical.sql
 #Adds Daily Market
 echo ADDING DAILY MARKET
 docker exec sqlserver /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Ih4teMicrosoft# -d dwh -i sql_files/add_daily_market_historical.sql
