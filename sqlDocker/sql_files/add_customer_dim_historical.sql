f2f4b7b4f068d9b0301f4cdbd929d0be5f0dc83c
Gabriel Lozano <golozanop@unal.edu.co>
2023-12-18 12:01:05 +0100

    kinda works

---
+++ sqlDocker/sql_files/add_customer_dim_historical.sql
@@ -0,0 +1,45 @@
+ -- Pending primary key
+ CREATE TABLE DimCustomer (
+    CustomerID NVARCHAR(80) ,
+    TaxId NVARCHAR(20) ,
+    Status NVARCHAR(10) ,
+    LastName NVARCHAR(30) ,
+    FirstName NVARCHAR(30) ,
+    MiddleInitial NVARCHAR(1) ,
+    Gender NVARCHAR(1) ,
+    Tier NUMERIC(1) ,
+    DOB DATE,
+    AddressLine1 NVARCHAR(80) ,
+    AddressLine2 NVARCHAR(80) ,
+    PostalCode NVARCHAR(12) ,
+    City NVARCHAR(25) ,
+    StateProv NVARCHAR(20) ,
+    Country NVARCHAR(24) ,
+    Phone1 NVARCHAR(30) ,
+    Phone2 NVARCHAR(30) ,
+    Phone3 NVARCHAR(30) ,
+    Email1 NVARCHAR(50) ,
+    Email2 NVARCHAR(50) ,
+    NationalTaxRateDesc NVARCHAR(50) ,
+    NationalTaxRate NUMERIC(6,5)  ,
+    LocalTaxRateDesc NVARCHAR(50) ,
+    LocalTaxRate NUMERIC(6,5)  ,
+    AgencyID NVARCHAR(30),
+    CreditRating NUMERIC(6),
+    NetWorth NUMERIC(11),
+    MarketingNameplate NVARCHAR(100),
+    IsCurrent BIT   ,
+    BatchID NUMERIC(5)  ,
+    EffectiveDate DATE ,
+    EndDate DATE ,
+    nationalTaxId NVARCHAR(80) ,
+    localTaxId NVARCHAR(80) 
+ )
+
+BULK INSERT DimCustomer FROM '/usr/config/data/gendata/Batch1/CustomerDim.csv' WITH
+(
+    FIRSTROW = 2,
+    FIELDTERMINATOR = ',',
+    ROWTERMINATOR = '0x0a',
+    KEEPNULLS
+)
