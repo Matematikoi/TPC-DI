8c2ee193c5442fb07b73f39ca1a0f40bdcafdac6
ngnushi <148450157+ngnushi@users.noreply.github.com>
2023-12-20 12:15:32 +0100

    Create add_fact_market_history_historical.sql

---
+++ sqlDocker/sql_files/add_fact_market_history_historical.sql
@@ -0,0 +1,67 @@
+WITH High_Low AS (
+	SELECT DM1.*, MIN(DM2.DM_DATE) AS FiftyTwoWeekHighDate, MIN(DM3.DM_DATE) AS FiftyTwoWeekLowDate
+    FROM
+	(
+		SELECT 
+			DM.DM_DATE, 
+			DM.DM_S_SYMB, 
+			DM.DM_CLOSE, 
+			DM.DM_HIGH, 
+			DM.DM_LOW, 
+			DM.DM_VOL, 
+			MAX(DM.DM_HIGH) OVER(PARTITION BY DM.DM_S_SYMB ORDER BY DM.DM_DATE ROWS BETWEEN 365 PRECEDING AND CURRENT ROW) AS FiftyTwoWeekHigh, 
+			MIN(DM.DM_LOW) OVER(PARTITION BY DM.DM_S_SYMB ORDER BY DM.DM_DATE ROWS BETWEEN 365 PRECEDING AND CURRENT ROW) AS FiftyTwoWeekLow
+		FROM raw.DailyMarket DM
+	   
+	) DM1
+	INNER JOIN raw.DailyMarket DM2
+		ON DM2.DM_HIGH = DM1.FiftyTwoWeekHigh
+		AND DM2.DM_DATE BETWEEN CONVERT(DATE, DATEADD(DAY, -365, DM1.DM_DATE)) AND DM1.DM_DATE
+	INNER JOIN raw.DailyMarket DM3
+		ON DM3.DM_LOW = DM1.FiftyTwoWeekLow
+		AND DM3.DM_DATE BETWEEN CONVERT(DATE, DATEADD(DAY, -365, DM1.DM_DATE)) AND DM1.DM_DATE
+	GROUP BY DM1.DM_DATE, DM1.DM_S_SYMB, DM1.DM_CLOSE, DM1.DM_HIGH, DM1.DM_LOW, DM1.DM_VOL, DM1.FiftyTwoWeekHigh, DM1.FiftyTwoWeekLow
+),
+
+TOTALEPS AS (
+	SELECT
+		CoNameOrCIK,
+		SUM(CAST(EPS AS FLOAT)) OVER(PARTITION BY Quarter ORDER BY Year, Quarter ROWS BETWEEN 4 PRECEDING AND CURRENT ROW) AS Total_EPS
+	FROM raw.FinwireFinancial
+),
+ 
+CompanyEarnings AS (
+	SELECT DISTINCT SK_CompanyID, Total_EPS
+	FROM DimCompany DC, TOTALEPS
+	WHERE ISNUMERIC(TOTALEPS.CoNameOrCIK) = 1
+	AND DC.CompanyID = CAST(TOTALEPS.CoNameOrCIK AS INT)
+	UNION
+	SELECT DISTINCT SK_CompanyID, Total_EPS
+	FROM DimCompany DC,  TOTALEPS
+	WHERE ISNUMERIC(TOTALEPS.CoNameOrCIK) = 0
+	AND DC.Name = TOTALEPS.CoNameOrCIK
+)
+
+SELECT
+	HL.DM_CLOSE AS ClosePrice, 
+	HL.DM_HIGH AS DayHigh, 
+	HL.DM_LOW AS DayLow,
+	HL.DM_VOL AS Volume,
+    DS.SK_SecurityID,
+    DS.SK_CompanyID,
+	DD1.SK_DateID AS SK_DateID,
+	HL.FiftyTwoWeekHigh,
+	DD2.SK_DateID AS SK_FiftyTwoWeekHighDate,
+	HL.FiftyTwoWeekLow,
+	DD3.SK_DateID AS SK_FiftyTwoWeekLowDate,
+	HL.DM_CLOSE / CE.Total_EPS AS PERatio,
+	DS.Dividend / HL.DM_CLOSE * 100 AS Yield, 
+	1 AS BatchID
+into FactMarketHistory
+FROM High_Low HL, raw.DailyMarket DM, DimSecurity DS, CompanyEarnings CE, DimDate DD1, DimDate DD2, DimDate DD3
+WHERE HL.DM_S_SYMB = DS.Symbol and DS.IsCurrent=1
+AND DS.SK_CompanyID = CE.SK_CompanyID
+AND HL.DM_DATE = DD1.DateValue
+AND HL.FiftyTwoWeekHighDate = DD2.DateValue
+AND HL.FiftyTwoWeekLowDate = DD3.DateValue
+AND DS.EffectiveDate <= DM.DM_DATE AND DM.DM_DATE < DS.EndDate
