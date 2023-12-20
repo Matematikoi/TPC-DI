733da13e2a1248f0cfaf34a4e39acf0bf720546f
ngnushi <148450157+ngnushi@users.noreply.github.com>
2023-12-20 12:11:56 +0100

    Create add_fact_cash_balances_historical.sql

---
+++ sqlDocker/sql_files/add_fact_cash_balances_historical.sql
@@ -0,0 +1,22 @@
+WITH CashBalancesTransformation AS (SELECT
+	DA.SK_CustomerID AS SK_CustomerID,
+	DA.SK_AccountID AS SK_AccountID,
+	DD.SK_DateID AS SK_DateID,
+	SUM(CT_AMT) OVER (PARTITION BY SK_AccountID ORDER BY DateValue) Cash
+FROM raw.CashTransaction CT, DimAccount DA, DimDate DD
+WHERE CT.CT_CA_ID = DA.AccountID
+	AND CONVERT(DATE, CT_DTS) BETWEEN DA.EffectiveDate AND DA.EndDate
+	AND CONVERT(DATE, CT_DTS) = DD.DateValue
+GROUP BY 
+	DA.SK_CustomerID,
+	DA.SK_AccountID,
+	DD.SK_DateID,
+	CONVERT(DATE, CT_DTS))
+SELECT 
+     SK_CUSTOMER_ID
+	,SK_ACCOUNT_ID
+	,SK_DATE_ID
+	,Cash
+	, 1 as BATCHID
+INSERT INTO FactCashBalances
+from CashBalancesTransformation
