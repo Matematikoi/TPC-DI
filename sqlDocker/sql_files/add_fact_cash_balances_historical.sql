713d0783c4f3e6da301abc96277262ace43664c3
ngnushi <148450157+ngnushi@users.noreply.github.com>
2023-12-21 19:24:23 +0100

    Update add_fact_cash_balances_historical.sql

--- sqlDocker/sql_files/add_fact_cash_balances_historical.sql
+++ sqlDocker/sql_files/add_fact_cash_balances_historical.sql
@@ -3,15 +3,15 @@ WITH CashBalancesTransformation AS (SELECT
 	DA.SK_AccountID AS SK_AccountID,
 	DD.SK_DateID AS SK_DateID,
 	SUM(CT_AMT) OVER (PARTITION BY SK_AccountID ORDER BY DateValue) Cash
-FROM raw.CashTransaction CT, DimAccount DA, DimDate DD
+FROM [dwh].[raw].[CashTransaction] CT, [dwh].[dbo].[DimAccount] DA, [dwh].[dbo]. [DimDate] DD
 WHERE CT.CT_CA_ID = DA.AccountID
 	AND CONVERT(DATE, CT_DTS) BETWEEN DA.EffectiveDate AND DA.EndDate
 	AND CONVERT(DATE, CT_DTS) = DD.DateValue
 )
 SELECT 
-     SK_CUSTOMER_ID
-	,SK_ACCOUNT_ID
-	,SK_DATE_ID
+         SK_CustomerID
+	,SK_AccountID
+	,SK_DateID
 	,Cash
 	, 1 as BATCHID
 INTO FactCashBalances
