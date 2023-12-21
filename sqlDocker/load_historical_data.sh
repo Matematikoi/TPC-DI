7e4028dbf26d67c16f150cd8ca126697e499fc4a
Jclozdib <31547235+Jclozdib@users.noreply.github.com>
2023-12-21 16:28:36 +0100

    Fixes issues with Company, Security, Trade + Adds FactHoldings (#14)
    Co-authored-by: Gabriel Lozano <golozanop@unal.edu.co>

--- sqlDocker/load_historical_data.sh
+++ sqlDocker/load_historical_data.sh
@@ -21,20 +21,23 @@ docker exec sqlserver /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Ih4teMic
 # Adds Status Type
 echo ADDING STATUS TYPE
 docker exec sqlserver /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Ih4teMicrosoft# -d dwh -i sql_files/add_status_type_historical.sql
+# Adds Trade Type
+echo ADDING TRADE TYPE
+docker exec sqlserver /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Ih4teMicrosoft# -d dwh -i sql_files/add_trade_type_historical.sql
+# Adds DimCustomer History
+echo ADDING CUSTOMER DIMENSION
+docker exec sqlserver /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Ih4teMicrosoft# -d dwh -i sql_files/add_customer_dim_historical.sql
+# Adds DimAccountHistory
+echo ADDING Dim Account
+docker exec sqlserver /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Ih4teMicrosoft# -d dwh -i sql_files/add_account_dim_historical.sql
 # Adds more dimensions
 echo ADDING SECURITY DIMENSION
 echo ADDING TRADE DIMENSION
 echo ADDING FINANCIAL
 docker exec sqlserver /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Ih4teMicrosoft# -d dwh -i sql_files/add_financial_trade_security_historical.sql
-# Adds Trade Type
-echo ADDING TRADE TYPE
-docker exec sqlserver /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Ih4teMicrosoft# -d dwh -i sql_files/add_trade_type_historical.sql
-# Adds DimCustomer History
-echo ADDING Dim Customer
-docker exec sqlserver /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Ih4teMicrosoft# -d dwh -i sql_files/add_customer_dim_historical.sql
-# Adds DimAccountHistory
-echo ADDING Dim Account
-docker exec sqlserver /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Ih4teMicrosoft# -d dwh -i sql_files/add_account_dim_historical.sql
+# Adds Fact Holdings
+echo ADDING FACT HOLDINGS
+docker exec sqlserver /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Ih4teMicrosoft# -d dwh -i sql_files/add_fact_holdings_historical.sql
 
 
 
